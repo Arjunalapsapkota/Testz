00d60da09a9698c84ca07ced3d8639a81c084001
arjunalapsapkota <arjunalapsapkota@gmail.com>
2019-02-22 15:48:23 -0800

    test18

---
+++ client/build/precache-manifest.cf4734edf013321715fbe56dab48f2be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c102a7de3f403c3a800b1e0d8a2fe5a1",
+    "url": "/static/media/beach-beach-chairs-blue-sky-1174732 copy.c102a7de.jpg"
+  },
+  {
+    "revision": "cda6dd52c7c5c94a091eda3aee296726",
+    "url": "/static/media/audio-dj-mix-63703 copy.cda6dd52.jpg"
+  },
+  {
+    "revision": "1bc6bfcb0f9c58ec5bfcb151936aaf95",
+    "url": "/static/media/abstract-art-background-1037999 copy.1bc6bfcb.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9e47ddb67118e13731d1",
+    "url": "/static/js/main.9e47ddb6.chunk.js"
+  },
+  {
+    "revision": "c9c4fb34041161ac2b2c",
+    "url": "/static/js/1.c9c4fb34.chunk.js"
+  },
+  {
+    "revision": "9e47ddb67118e13731d1",
+    "url": "/static/css/main.322c9aae.chunk.css"
+  },
+  {
+    "revision": "f00776ebe86d33dc73f8c820a17e4610",
+    "url": "/index.html"
+  }
+];
